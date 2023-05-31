0ba1d4804855d511bd3ade4d96f89327297787cd
XixWISExiX <addalooptoit@gmail.com>
2023-05-31 09:21:13 -0500

    Added dynamic change to sidebar height

---
+++ DOMref.js
@@ -0,0 +1,4 @@
+const body = document.querySelector("body");
+const sidebar = body.querySelector(".sidebar");
+const logo = body.querySelector(".logo");
+export { body, sidebar, logo };
--- script.js
+++ script.js
@@ -1,19 +1,39 @@
+import { body, sidebar, logo } from "./DOMref.js";
+
+function application() {
+  sidebarHeightResize();
+}
+
 let library = (function () {
   let bookList = [];
   return { bookList };
 })();
 
-function Book(book) {
-  this.book = book;
+// EXTRA CODE WHICH CAN BE BUILT UPON, BUT NOT CURRENTLY NEEDED
+// function Book(book) {
+//   this.book = book;
+// }
+
+// function addBookToLibrary(book) {
+//   //import
+//   let book = new Book("");
+//   library.bookList.push("oj");
+// }
+
+// let boo = new Book("dog");
+
+// library.bookList.push(boo.book);
+// console.log(library.bookList);
+
+function sidebarHeightResize() {
+  window.addEventListener("resize", function () {
+    const logoHeight = logo.offsetHeight;
+    const viewportHeight = window.innerHeight;
+    const remainingHeight = viewportHeight - logoHeight * 1.45;
+    sidebar.style.height = `${remainingHeight}px`;
+  });
+  // Trigger the resize event initially to set the sidebar height on page load
+  window.dispatchEvent(new Event("resize"));
 }
 
-function addBookToLibrary(book) {
-  //import
-  let book = new Book("");
-  library.bookList.push("oj");
-}
-
-let boo = new Book("dog");
-
-library.bookList.push(boo.book);
-console.log(library.bookList);
+document.addEventListener("DOMContentLoaded", application);
